79ff4eaad077a796b28c08ecce4cf8dec0c822b7
guomin <yushi5344@gmail.com>
2018-06-05 22:01:13 +0800

    信号触发

---
+++ signal.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: yushi
+	 * Date: 2018/6/5
+	 * Time: 21:57
+	 */
+	//触发函数 异步执行 达到10次 停止
+	swoole_process:sign(SIGALRM,function (){
+	static $i=0;
+	echo "$i \n";
+	$i++;
+	if ($i>10){
+		swoole_process::alarm(-1);//清除定时器
+	}
+});
+
+	//定时信号
+	swoole_process::alarm(100*1000);
